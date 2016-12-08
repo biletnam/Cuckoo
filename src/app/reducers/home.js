d1fc1824501b134cd16e7e8de6dbc2007fd1b670
xiaoyuhen <xiaoyuhen1994@gmail.com>
2016-12-09 01:36:05 +0800

    :construction: add real boxoffice data

--- src/app/reducers/home.js
+++ src/app/reducers/home.js
@@ -2,14 +2,14 @@ import { handleActions } from 'redux-actions';
 import * as types from '../constants/ActionTypes';
 
 const initialState = {
-  data: 0,
+  boxoffice: {},
 };
 
 const handler = {};
 
 handler[types.GET_MOVIE_REVENUES] = (state, action) => {
   return Object.assign({}, state, {
-    data: action.data,
+    boxoffice: action.boxoffice,
   });
 };
 
