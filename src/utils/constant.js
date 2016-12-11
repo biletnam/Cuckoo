291abda7f4b3921febe86fd3c59b9321a3594309
xiaoyuhen <xiaoyuhen1994@gmail.com>
2016-12-11 15:56:27 +0800

    :sparkles: add boxoffice table

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,21 @@
-export const FooterType = {
+export const FOOTER_TYPE = {
   0: 'boxoffice',
   1: 'production',
   2: 'news',
   3: 'calendar',
 };
+
+export const BOXOFFICE_TABLE_TITLE = [
+  '影片名称',
+  '实时票房',
+  '排片占比',
+  '上座率',
+  '累计票房',
+];
+
+export const PIE_COLORS = [
+  '#FF5937',
+  '#FDAF29',
+  '#1985CD',
+  '#4D5E70',
+];
